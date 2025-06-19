42f06eecfccb9e12598813866bc88aef20af03c0
Rasheek <sallanrasheek@gmail.com>
2025-06-20 04:24:50 +0530

    added protected routes and custom method for pretected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,26 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoutes = createRouteMatcher([
+  "/dashboard(.*)",
+  "/api/payment",
+  "/payment(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoutes(req)) {
+    const sessionAuth = await auth();
+    if (!sessionAuth.userId) {
+      // You can customize the response as needed (e.g., redirect or throw)
+      return Response.redirect("/sign-in");
+    }
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
