d6a065e515e448b0661f5fff324814c2cae6a5e7
Rasheek <sallanrasheek@gmail.com>
2025-06-20 04:32:44 +0530

    wrapped children in theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const menrope = Manrope({
   subsets: ["latin"],
@@ -21,7 +22,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${menrope.className} bg-[#171717] antialiased`}>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
