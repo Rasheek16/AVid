6ba457c2ec3491b6400f3443f9a5a5123cda74b8
Rasheek <sallanrasheek@gmail.com>
2025-06-27 23:03:25 +0530

    signup page

---
+++ src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from "@clerk/nextjs";
+
+type Props = {};
+const SignUpPage = (props: Props) => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+export default SignUpPage;
