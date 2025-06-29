0dc1d5becc5f706b20b3060cc0b8ba8b2e5cc4d9
Rasheek <sallanrasheek@gmail.com>
2025-06-29 19:35:53 +0530

    dashboard template

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+const DashboardPage = (props: Props) => {
+  // Authentication
+
+  // Check if the user is authenticed and exists
+  // if not redirect to the login/sign-in page
+  return <div>DashboardPage</div>;
+};
+export default DashboardPage;
