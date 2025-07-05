80eeaa31cc2d885ef61c16640e1cb48475ea7634
Rasheek <sallanrasheek@gmail.com>
2025-07-05 08:53:03 +0530

    onAuthenticatedUser

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,8 +1,9 @@
 "use server";
 
-import { currentUser } from "@clerk/nextjs/server";
+import { Client, currentUser } from "@clerk/nextjs/server";
 import { use } from "react";
-
+import { client } from "@/lib/prisma";
+import { User } from "lucide-react";
 export const onAuthenticateUser = async () => {
   try {
     const user = await currentUser();
@@ -12,10 +13,69 @@ export const onAuthenticateUser = async () => {
       return { status: 403 };
     }
 
-    // const exisitingUser = await db.user.findUnique({
-    //   where: {
-    //     clerkId: user.id,
-    //   },
-    // });
-  } catch (error) {}
+    const userExist = await client.user.findUnique({
+      where: {
+        clerkid: user.id,
+      },
+      include: {
+        workspace: {
+          where: {
+            User: {
+              clerkid: user.id,
+            },
+          },
+        },
+      },
+    });
+
+    if (userExist) {
+      return {
+        status: 200,
+        user: userExist,
+      };
+    }
+    const newUser = await client.user.create({
+      data: {
+        firstname: user.firstName,
+        lastname: user.lastName,
+        image: user.imageUrl,
+        clerkid: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        studio: {
+          create: {},
+        },
+        subscription: {
+          create: {},
+        },
+        workspace: {
+          create: {
+            name: `${user.firstName}'s Workspace`,
+            type: "PERSONAL",
+          },
+        },
+      },
+      include: {
+        workspace: {
+          User: {
+            clerid: user.id,
+          },
+        },
+      },
+      subscription: {
+        select: {
+          plan: true,
+        },
+      },
+    });
+    if (newUser) {
+      return {
+        status: 200,
+        user: newUser,
+      };
+    }
+
+    return { status: 400 };
+  } catch (error) {
+    return { status: 500 };
+  }
 };
